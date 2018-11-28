cf719e661c819f2c4d518a18b18f85f4d64e370e
slick1537 <kevin.m.mckinney@gmail.com>
2018-11-28 18:38:38 -0500

    Added numOfRows PDM, cleaned up formatting and inserted more comments to make purpose of each method more clear.

--- grid.py
+++ grid.py
@@ -1,76 +1,98 @@
 class Grid:
-  def __init__( self ):
-    self.grid = []
-    self.depend =  {
-        "a0": 0,
-        "a1": 0,
-        "a2": 0,
-        "a3": 0,
-        "s0": 0,
-        "s1": 0,
-        "s2": 0,
-        "s3": 0,
-        "s4": 0,
-        "s5": 0,
-        "s6": 0,
-        "s7": 0,
-        "t0": 0,
-        "t1": 0,
-        "t2": 0,
-        "t3": 0,
-        "t4": 0,
-        "t5": 0,
-        "t6": 0,
-        "t7": 0,
-        "t8": 0,
-        "t9": 0,
-        "zero": 0,
-        "v0": 0,
-        "v1": 0
-    }
-    self.value =  {
-        "a0": 0,
-        "a1": 0,
-        "a2": 0,
-        "a3": 0,
-        "s0": 0,
-        "s1": 0,
-        "s2": 0,
-        "s3": 0,
-        "s4": 0,
-        "s5": 0,
-        "s6": 0,
-        "s7": 0,
-        "t0": 0,
-        "t1": 0,
-        "t2": 0,
-        "t3": 0,
-        "t4": 0,
-        "t5": 0,
-        "t6": 0,
-        "t7": 0,
-        "t8": 0,
-        "t9": 0,
-        "zero": 0,
-        "v0": 0,
-        "v1": 0
-    }
+    def __init__(self):
+        self.grid = []              # 2d array which holds each row to be printed
+        self.numOfRows = 0          # contains the number of rows in grid
+        self.depend = {             # Dictionary which contains number of dependencies on each register
+            "a0": 0,
+            "a1": 0,
+            "a2": 0,
+            "a3": 0,
+            "s0": 0,
+            "s1": 0,
+            "s2": 0,
+            "s3": 0,
+            "s4": 0,
+            "s5": 0,
+            "s6": 0,
+            "s7": 0,
+            "t0": 0,
+            "t1": 0,
+            "t2": 0,
+            "t3": 0,
+            "t4": 0,
+            "t5": 0,
+            "t6": 0,
+            "t7": 0,
+            "t8": 0,
+            "t9": 0,
+            "zero": 0,
+            "v0": 0,
+            "v1": 0
+        }
+        self.value = {              # Dictionary which contains the value stored in each register
+            "a0": 0,
+            "a1": 0,
+            "a2": 0,
+            "a3": 0,
+            "s0": 0,
+            "s1": 0,
+            "s2": 0,
+            "s3": 0,
+            "s4": 0,
+            "s5": 0,
+            "s6": 0,
+            "s7": 0,
+            "t0": 0,
+            "t1": 0,
+            "t2": 0,
+            "t3": 0,
+            "t4": 0,
+            "t5": 0,
+            "t6": 0,
+            "t7": 0,
+            "t8": 0,
+            "t9": 0,
+            "zero": 0,
+            "v0": 0,
+            "v1": 0
+        }
 
+        # Insert line into grid - Sebastian
+        # Inputs: line to be inserted into grid
+        # Outputs: None
+        def insertLine(line):
 
-    # Insert line into grid - Sebastian
-    def insertLine( self, line ):
-        return
+            pass
 
-    # Prints out in the grid - Joe
-    def printGrid( self ):
-        return
+        # Prints out in the grid - Joe
+        # Input: cycle - The cycle number we are print (to determine visibility)
+        # Outputs: Prints the output for this cycle of the simulation
+        def printGrid(cycle):
 
+            pass
 
-    # Shifts arrays around to insert nop where needed
-    def resolveHazard( self, depend, registers, forward ):
-        return
+        # Shifts arrays around to insert nop where needed
+        # Inputs: None
+        # Outputs: Updates grid to correct for hazards
+        def resolveHazard():
 
+            pass
 
-    # Goes through line and returns what the instruction is and what each operation is - Kevin
-    def stripLine( self ):
-        return
+        # Goes through line and returns what the instruction is and what each operation is - Kevin
+        # Inputs: instruction string
+        #
+        # Outputs:
+        #           For instructions in the form of "instruction a,b,c"
+        #           output1 - instruction
+        #           output2 - operand a
+        #           output3 - operand b
+        #           output4 - operand c
+        #
+        #           For instructions in the form of "instruction $a,offset($b)"
+        #           output1 - instruction
+        #           output2 - operand a
+        #           output3 - operand b
+        #           output4 - NoneType (since there are only two registers used in this case)
+        def stripLine(instruction):
+
+            pass
